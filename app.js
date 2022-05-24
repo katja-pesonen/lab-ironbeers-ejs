f742e09f692ddaa289da5d2a80a2a507cc372229
Katja Pesonen <katjapesonen1@gmail.com>
2022-05-24 23:48:19 +0300

    done

--- app.js
+++ app.js
@@ -15,8 +15,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', (req, res) => {  
+  res.render('index'); 
 });
+
+app.get('/beers',  async (req, res) => {
+  const beersFromApi = await punkAPI.getBeers();
+  const first25 = beersFromApi.slice(0,25);
+  // console.log(beersFromApi.slice(0,25));
+  res.render('beers', { first25 });
+});
+
+
+app.get('/random-beer',  (req, res) => {
+  const randomBeer = punkAPI.getRandom();
+  console.log("Random Beer", randomBeer);
+  randomBeer.then(() => {
+    res.render('random-beer', { randomBeer });
+    randomBeer.catch(err => { console.log(err) })
+  })
+});
+
+
+// app.get('/beers', (req, res) => {  res.render('beers.ejs') });
+// app.get('/random-beer', (req, res) => {  res.render('index') });
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
